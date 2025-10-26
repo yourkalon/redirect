9c6161a3630a03739fad33111ae324efa026e63c
yourkalon <165014118+yourkalon@users.noreply.github.com>
2025-10-26 13:44:57 +0800

    Update ps.js

--- ps.js
+++ ps.js
@@ -1,1 +1,7 @@
-window.location.replace("https://YOUR-NEW-URL.com/");
+var links = [
+    "https://link1.com",
+    "https://link2.com",
+    "https://link3.com"
+  ];
+  var random = Math.floor(Math.random() * links.length);
+  window.location.replace(links[random]);
