9ab79383620d5b6baea9727bdeb16d4587376e9c
yourkalon <165014118+yourkalon@users.noreply.github.com>
2025-10-26 14:02:06 +0800

    Update ps.js

--- ps.js
+++ ps.js
@@ -1,7 +1,7 @@
 var links = [
-    'https://cutl.io/link1-22-07-25',
-    'https://cutl.io/link2-22-07-25',
-    'https://cutl.io/link3-22-07-25'
+    'https://link1.com',
+    'https://link2.com',
+    'https://link3.com'
 ];
 var randomIndex = Math.floor(Math.random() * links.length);
 var selectedLink = links[randomIndex];
