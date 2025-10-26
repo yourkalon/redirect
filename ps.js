bbc09cb6fda8b426a247e4888c450b324a664a71
yourkalon <165014118+yourkalon@users.noreply.github.com>
2025-10-26 13:59:35 +0800

    Update ps.js

--- ps.js
+++ ps.js
@@ -1,7 +1,8 @@
 var links = [
-    "https://link1.com",
-    "https://link2.com",
-    "https://link3.com"
-  ];
-  var random = Math.floor(Math.random() * links.length);
-  window.location.replace(links[random]);
+    'https://cutl.io/link1-22-07-25',
+    'https://cutl.io/link2-22-07-25',
+    'https://cutl.io/link3-22-07-25'
+];
+var randomIndex = Math.floor(Math.random() * links.length);
+var selectedLink = links[randomIndex];
+window.location.href = selectedLink;
